5f8290356d9ef2f5605b831ad7410badfeb64f28
codeurient <codeurient@gmail.com>
2025-04-14 22:41:44 +0400

    route group

--- routes/web.php
+++ routes/web.php
@@ -2,31 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('about', function () {
-    return "<h1>About Page</h1>";
-})->name('example');
 
-Route::get('contact', function () {
-    return "<h1>Contact Page</h1>";
-});
+Route::group(['prefix'=>'customer'], function(){
+    Route::get('/', function(){
+        return "<h1>Customer</h1>";
+    }); 
 
-Route::get('contact/{id}', function ($id) {
-    return $id;
-})->name('edit-contact');
+    Route::get('/create', function(){
+        return "<h1>Customer create</h1>";
+    }); 
 
-
-
-
-Route::get('/user/{id}/{slug}', function ($id, $slug) {
-    return $id . ' ' . $slug;
-})->name('user.post');
-
-
-Route::get('home', function () {
-    return 
-        "
-            <a href='".route('example')."'>About Page</a>
-            <a href='".route('edit-contact', 12)."'>Dynamic Page</a>
-            <a href='".route('user.post', ['id' => 1, 'slug' => 'hello'])."'>More parameters</a>
-        ";
+    Route::get('/show', function(){
+        return "<h1>Customer show</h1>";
+    }); 
 });
