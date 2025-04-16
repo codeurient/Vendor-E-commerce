c93d16f09307447fd29049f5e963cceab9299869
codeurient <codeurient@gmail.com>
2025-04-17 00:22:59 +0400

    Basic controllers

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class HomeController extends Controller
+{
+    public function index() {
+        $blogs = [
+            [
+                'title' => 'Afterward, once I became famous',
+                'body' => 'I once did an interview for the Banbury Herald. I must look it out one of these days, for the biography. Strange chap they sent me. A boy, really.',
+                'status' => 1
+            ],
+            [
+                'title' => 'And I told. Simple little',
+                'body' => ' The collar, the cut, the fabric, all wrong. It was the kind of thing a mother might buy for a boy leaving school for his first job, imagining that her child will somehow grow into it.',
+                'status' => 1
+            ],
+            [
+                'title' => 'They went away happy',
+                'body' => 'Apart from the fact that they make dull companions. Just so long as they don"t start on about storytelling and honesty, the way some of them do. Naturally that annoys me.',
+                'status' => 0
+            ],
+            [
+                'title' => 'Anyway, the boy from the',
+                'body' => 'They would do research, come along with a little piece of truth concealed in their pocket, draw it out at an opportune moment and hope to startle me into revealing more.',
+                'status' => 1
+            ],
+        ];
+
+        return view('home', compact('blogs'));
+    }
+}
