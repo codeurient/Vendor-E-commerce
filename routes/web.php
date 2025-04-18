cd4bdfc2777af7bf2e0f8654e1224dc4a107a5e9
codeurient <codeurient@gmail.com>
2025-04-19 00:20:44 +0400

    Form Markup

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,15 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\BlogController;
-use App\Http\Controllers\HomeController;
 use App\Http\Controllers\AboutController;
-
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LoginController;
 
 Route::get('/',         HomeController::class)->name('home'); 
 
 
-
+Route::get('/login',    [LoginController::class, 'index'])->name('login'); 
 Route::get('/about',    [AboutController::class, 'index'])->name('about'); 
 
-Route::resource('blog', BlogController::class);
+
+
