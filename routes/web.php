d5cfc0b2da7fa56d5a60c3755b7806eddf9c58fe
codeurient <codeurient@gmail.com>
2025-04-18 20:14:10 +0400

    Resource controllers

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,12 @@
 <?php
 
-use App\Http\Controllers\AboutController;
-use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BlogController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AboutController;
 
 
-Route::get('/',      [HomeController::class,  'index'])->name('home'); 
-Route::get('/about', [AboutController::class, 'index'])->name('about'); 
+Route::get('/',         [HomeController::class,  'index'])->name('home'); 
+Route::get('/about',    [AboutController::class, 'index'])->name('about'); 
 
+Route::resource('blog', BlogController::class);
