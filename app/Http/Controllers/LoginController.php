9564ea744676912c9a1fe92bb0bc194fa87d6850
codeurient <codeurient@gmail.com>
2025-04-20 19:43:48 +0400

    More Validation Rules

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -13,8 +13,9 @@ class LoginController extends Controller
     public function handleLogin(Request $request) {
 
         $request->validate([
-            'name' => 'required',
-            'email' => 'required',
+            'name' => ['required', 'alpha', 'min:6', 'max:10'],
+            // 'name' => 'required|alpha',
+            'email' => ['required', 'email'],
             'password' => 'required'
         ]);
         
