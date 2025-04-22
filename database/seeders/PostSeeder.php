9391abf31274267717890a1d09129290eadf741e
codeurient <codeurient@gmail.com>
2025-04-22 13:51:35 +0400

    factory + seed

--- database/seeders/PostSeeder.php
+++ database/seeders/PostSeeder.php
@@ -1,32 +1,17 @@
 <?php
 
 namespace Database\Seeders;
+use App\Models\Post;
+use Faker\Factory as Faker;
 use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use Faker\Factory as Faker;
 
 class PostSeeder extends Seeder
 {
     public function run(): void
     {
-        // DB::table('posts')->insert([
-        //     'title' => Str::random(20),
-        //     'description' => Str::random(200),
-        //     'status' => 1,
-        //     'publish_date' => date('Y-m-d'),
-        //     'user_id' => 1,
-        // ]);
-
-        $faker = Faker::create();
-
-        DB::table('posts')->insert([
-            'title' => $faker->sentence(),
-            'description' => $faker->paragraph(),
-            'status' => 1,
-            'publish_date' => $faker->date(),
-            'user_id' => 1,
-        ]);
+        Post::factory()->count(10)->create(); // 10 post yaradır
     }
 }
