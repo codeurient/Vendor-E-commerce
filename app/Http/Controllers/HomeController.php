2eda7cabe6b77328c3e4e80957ec994fbc1894fd
codeurient <codeurient@gmail.com>
2025-04-23 23:17:51 +0400

    delete

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,13 +9,7 @@ class HomeController extends Controller
 {
     public function __invoke(Request $request)
     {
-        DB::table('posts')->where('id', 17)->update([
-            'title' => 'This is a UPDATED test Data 1',
-            'description' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum, ipsa.',
-            'status' => 0,
-            'publish_date' => date('Y-m-d'),
-            'user_id' => 1
-        ]);
+        DB::table('posts')->where('id', 16)->delete();
 
         dd('success');
     }
