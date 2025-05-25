b2297787e06bc23a8d44bc643feb6db6d0003fa9
codeurient <codeurient@gmail.com>
2025-05-25 18:33:53 +0400

    passing data

---
+++ resources/views/components/post/index.blade.php
@@ -0,0 +1,10 @@
+<div class="col-md-4 ">
+    <div class="card h-100">
+        <img src="{{ asset($post->image) }}" alt="" style="width:100%; height:100%;" class="rounded">
+
+        <div class="card-body">
+            <h4>{{ $post->title }}</h4>
+            <p>{{ $post->description }}</p>
+        </div>
+    </div>
+</div>
