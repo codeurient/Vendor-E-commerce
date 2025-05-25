e5a4593e5622ecd7e5033e57c29810825eaee257
codeurient <codeurient@gmail.com>
2025-05-25 09:41:19 +0400

    rendering component

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,17 @@
+<div>
+    <h1>Contact</h1>
+
+    <x-button />
+
+
+
+    <x-forms.button />
+    
+    <x-forms.submit />
+
+
+     <x-input-field />
+     
+     <x-inputField />
+
+</div>
--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 
-
 Route::group(['middleware' => 'authCheck2'], function() {
 
     Route::get('/dashboard', function() {
@@ -15,25 +14,25 @@ Route::group(['middleware' => 'authCheck2'], function() {
 
 });
 
-
-
 Route::get('/unavailable', function() {
     return view('unavailable');
 })->name('unavailable');
-
 
 
 Route::get('/welcome', function() {
     return view('welcome');
 })->name('welcome');
 
-
 Route::get('/posts/trash',                  [PostController::class, 'trashed'])->name('posts.trashed');
-
 Route::get('/posts/{id}/restore',           [PostController::class, 'restore'])->name('posts.restore');
-
 Route::delete('/posts/{id}/force-delete',   [PostController::class, 'forceDelete'])->name('posts.force_delete');
 
-
-
 Route::resource('posts', PostController::class);
+
+
+
+Route::get('contact', function() {
+
+    return view('contact');
+
+});
