05e951ce3f45d47cf8f43cf3ca7f7b31a101226f
codeurient <codeurient@gmail.com>
2025-05-28 00:16:12 +0400

    Storing Data In Cache

---
+++ .lessons/26 Fundamental Cache/2 Storing Data In Cache/code/resources/views/components/post/index.blade.php
@@ -0,0 +1,13 @@
+<div class="col-md-4 ">
+    <div class="card h-100">
+        <img src="{{ asset($image) }}" alt="" style="width:100%; height:100%;" class="rounded">
+
+        <div class="card-body">
+            <h4>{{ $title }}</h4>
+            <p>{{ $description }}</p>
+
+            <span>{{ $site_name }}</span>
+            
+        </div>
+    </div>
+</div>
