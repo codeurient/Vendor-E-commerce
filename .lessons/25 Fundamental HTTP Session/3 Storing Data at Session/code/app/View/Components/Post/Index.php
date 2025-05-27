6cb7196b75ce962ee0b36a8766f4949617afd25d
codeurient <codeurient@gmail.com>
2025-05-27 15:58:54 +0400

    code

---
+++ .lessons/25 Fundamental HTTP Session/3 Storing Data at Session/code/app/View/Components/Post/Index.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\View\Components\Post;
+
+use Closure;
+use Illuminate\Contracts\View\View;
+use Illuminate\View\Component;
+
+class Index extends Component
+{
+    // public $post;
+
+    public function __construct()
+    {
+    //    $this->post = $post;
+    }
+
+
+    public function render(): View|Closure|string
+    {
+        return view('components.post.index');
+    }
+}
