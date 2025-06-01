08d19cb68f0a34b588f26c314a45b3b352370233
codeurient <codeurient@gmail.com>
2025-06-01 19:34:11 +0400

    Protecting Routes

---
+++ .lessons/28 Fundamental Authentication/5 Protecting Routes/code/resources/views/index.blade.php
@@ -0,0 +1,68 @@
+@extends('layouts.master')
+
+@section('content')
+    <div class="main-content mt-5">
+        <div class="card">
+            <div class="card-header mb-4">
+                <div class="row">
+                    <div class="col-md-6">
+                        <h4>All Posts</h4>
+                    </div>
+
+                    <div class="col-md-6 d-flex justify-content-end">
+                        <a class="btn btn-success mx-1" href="{{ route('posts.create') }}">Create <i class="fa-solid fa-plus"></i></a>
+
+                        <a class="btn btn-warning mx-1" href="{{ route('posts.trashed') }}">Trashed <i class="fa-solid fa-trash"></i></a>
+
+                    </div>
+                </div>
+            </div>
+
+            <div class="card-body">
+                <table class="table table-striped table-bordered border-dark">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col" style="width: 10%">Image</th>
+                            <th scope="col" style="width: 20%">Title</th>
+                            <th scope="col" style="width: 30%">Description</th>
+                            <th scope="col" style="width: 10%">Category</th>
+                            <th scope="col" style="width: 10%">Publish Date</th>
+                            <th scope="col" style="width: 20%">Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($posts as $post)
+                            <tr>
+                                <th scope="row">{{ $post->id }}</th>
+                                <td> <img width="70" height="70" src="{{ asset($post->image) }}" alt=""> </td>
+                                <td>{{ $post->title }}</td>
+                                <td>{{ $post->description }}</td>
+
+                                <td>{{ $post->category->name }}</td>
+
+                                <td>{{ date('d-m-Y', strtotime($post->created_at)) }}</td>
+                                
+                                <td>
+                                    <a class="btn-sm btn btn-success" href="{{ route('posts.show', $post->id)  }}">Show</a>
+
+                                    <a class="btn-sm btn btn-primary" href="{{ route('posts.edit', $post->id) }}">Edit</a>
+                                    {{-- <a class="btn-sm btn btn-danger" href="">Delete</a> --}}
+
+                                    <form action="{{ route('posts.destroy', $post->id) }}" method="POST" class="d-inline">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button class="btn-sm btn btn-danger">Delete</button>
+                                    </form>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+
+                {{ $posts->links() }}
+
+            </div>
+        </div>
+    </div>
+@endsection
---
+++ .lessons/28 Fundamental Authentication/5 Protecting Routes/code/resources/views/profile.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.master')
+
+
+@section('content')
+
+    <h1>This is a Profile!</h1>
+
+    <p>{{ $site_name }}</p>
+    
+@endsection
