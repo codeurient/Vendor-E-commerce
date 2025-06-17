0e0f9138c14352842c49f2df32a4a4e4c4983567
codeurient <codeurient@gmail.com>
2025-06-17 15:45:29 +0400

    Add datatable export buttons

---
+++ .lessons/34 Packages Yajra Datatable/4 Add datatable export buttons/code/routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Models\User;
+use App\DataTables\UsersDataTable;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
+
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get('/dashboard', function (UsersDataTable $dataTable) {
+    
+    return $dataTable->render('dashboard');
+
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
