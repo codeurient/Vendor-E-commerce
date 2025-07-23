2bca172f29fb409fbf9222907d889aa63510da5c
codeurient <codeurient@gmail.com>
2025-07-24 00:59:57 +0400

    Multi auth - Create separate route files for admin and vendor

---
+++ .lessons/48 Ecommerce - Multi Auth Setup With Breeze/3 Multi auth - Create dashboards for the users/code/routes/web.php
@@ -0,0 +1,27 @@
+<?php
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Backend\AdminController;
+use App\Http\Controllers\Backend\VendorController;
+
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
+
+
+Route::get('admin/dashboard',  [AdminController::class, 'dashboard'])->middleware('auth')->name('admin.dashboard');
+Route::get('vendor/dashboard', [VendorController::class, 'dashboard'])->middleware('auth')->name('vendor.dashboard');
