fec42c0d9d7b188f7d1e659bb4d5f83fcf9e9026
codeurient <codeurient@gmail.com>
2025-07-25 00:01:27 +0400

    Admin Panel - Admin login page mastering

---
+++ .lessons/49 Admin Panel Setup And Mastering/4 Admin Panel - Admin login page mastering/code/routes/admin.php
@@ -0,0 +1,6 @@
+<?php 
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Backend\AdminController;
+
+Route::get('dashboard',  [AdminController::class, 'dashboard'])->name('dashboard');
+
