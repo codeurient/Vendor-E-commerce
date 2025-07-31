d6dc28efcfc0ccb8eb5c26983a4a140805d5cd9b
codeurient <codeurient@gmail.com>
2025-07-31 19:29:28 +0400

    Frontend Setup- Master forget password page

---
+++ .lessons/51 Frontend Template Setup And Mastering/4 Frontend Setup- Master forget password page/code/routes/admin.php
@@ -0,0 +1,24 @@
+<?php 
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Backend\AdminController;
+use App\Http\Controllers\Backend\ProfileController;
+
+use App\Http\Controllers\Admin\Auth\AdminAuthenticatedSessionController;
+
+
+Route::middleware('guest.admin')->group(function () {
+    Route::get('login', [AdminAuthenticatedSessionController::class, 'create'])->name('login');
+    Route::post('login', [AdminAuthenticatedSessionController::class, 'store'])->name('login.submit');
+});
+
+
+Route::middleware(['auth', 'role:admin'])->group(function () {
+    Route::get('dashboard',  [AdminController::class, 'dashboard'])->name('dashboard');
+
+    Route::get('profile',                    [ProfileController::class, 'index'])->name('profile');
+    
+    Route::post('profile/update',            [ProfileController::class, 'updateProfile'])->name('profile.update');
+    Route::post('profile/update/password',   [ProfileController::class, 'updatePassword'])->name('password.update');
+    
+    Route::post('logout',    [AdminAuthenticatedSessionController::class, 'destroy'])->name('logout');
+});
