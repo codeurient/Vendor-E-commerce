2e3af7d2a39e3e5436e8e36d250a09fb8978aa71
codeurient <codeurient@gmail.com>
2025-08-03 13:42:01 +0400

    Vendor Panel Setup

---
+++ .lessons/52 Vendor Panel Setup/code/routes/web.php
@@ -0,0 +1,32 @@
+<?php
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Backend\AdminController;
+use App\Http\Controllers\Frontend\HomeController;
+use App\Http\Controllers\Frontend\UserDashboardController;
+
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', [HomeController::class, 'index'])->name('home');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
+
+// Route::get('admin/login', [AdminController::class, 'login'])->name('admin.login');
+
+
+
+
+Route::group(['middleware' => ['auth', 'verified'], 'prefix' => 'user',  'as' => 'user.'  ],    function(){
+
+    Route::get('dashboard', [UserDashboardController::class, 'index'])->name('dashboard');
+
+});
