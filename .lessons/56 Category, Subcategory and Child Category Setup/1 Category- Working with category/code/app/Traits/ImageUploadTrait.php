f3e9a7dc1a77c4cc2cb7497c24e024f7919a765b
codeurient <codeurient@gmail.com>
2025-08-10 15:28:02 +0400

    Category- Working with category

---
+++ .lessons/56 Category, Subcategory and Child Category Setup/1 Category- Working with category/code/app/Traits/ImageUploadTrait.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Traits;
+
+use Illuminate\Http\Request;
+use File;
+
+
+trait ImageUploadTrait {
+
+    public function uploadImage(Request $request, $inputName, $path) 
+    {
+        if($request->hasFile($inputName)) {
+            $image = $request->{$inputName};
+            // $imageName = rand().'_'.$image->getClientOriginalName();
+
+            $ext = $image->getClientOriginalExtension();
+            $imageName = 'media_'.uniqid().'.'. $ext;
+
+            $image->move(public_path($path), $imageName);
+            return $path.'/'.$imageName;
+        }
+    }
+
+    public function updateImage(Request $request, $inputName, $path,  $oldPath = null) 
+    {
+        if($request->hasFile($inputName)) {
+
+            if(File::exists(public_path($oldPath))) {
+                File::delete(public_path($oldPath));
+            }
+
+            $image = $request->{$inputName};
+            // $imageName = rand().'_'.$image->getClientOriginalName();
+
+            $ext = $image->getClientOriginalExtension();
+            $imageName = 'media_'.uniqid().'.'. $ext;
+
+            $image->move(public_path($path), $imageName);
+            return $path.'/'.$imageName;
+        }
+    }
+
+    public function deleteImage(?string $path) {
+        if ($path && File::exists(public_path($path))) {
+            File::delete(public_path($path));
+        }
+    }
+
+}
---
+++ app/Http/Controllers/Backend/CategoryController.php
@@ -0,0 +1,44 @@
+<?php
+namespace App\Http\Controllers\Backend;
+
+use App\Http\Controllers\Controller;
+use App\DataTables\CategoryDataTable;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    public function index(CategoryDataTable $dataTable)
+    {
+        return $dataTable->render('admin.category.index');
+    }
+
+    public function create()
+    {
+        
+    }
+
+    public function store(Request $request)
+    {
+        
+    }
+
+    public function show(string $id)
+    {
+        
+    }
+
+    public function edit(string $id)
+    {
+        
+    }
+
+    public function update(Request $request, string $id)
+    {
+        
+    }
+
+    public function destroy(string $id)
+    {
+        
+    }
+}
