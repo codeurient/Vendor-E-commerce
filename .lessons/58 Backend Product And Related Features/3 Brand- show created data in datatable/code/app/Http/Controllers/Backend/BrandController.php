1c98827e6436e7593e9601d24a01887568e9af39
codeurient <codeurient@gmail.com>
2025-09-08 14:02:46 +0400

    Brand- show created data in datatable

---
+++ .lessons/58 Backend Product And Related Features/3 Brand- show created data in datatable/code/app/Http/Controllers/Backend/BrandController.php
@@ -0,0 +1,69 @@
+<?php
+namespace App\Http\Controllers\Backend;
+
+use Str;
+use App\Models\Brand;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Traits\ImageUploadTrait;
+use App\DataTables\BrandDataTable;
+
+class BrandController extends Controller
+{
+    use ImageUploadTrait;
+    
+    public function index(BrandDataTable $dataTable)
+    {
+        return $dataTable->render('admin.brand.index');
+    }
+
+    public function create()
+    {
+        return view('admin.brand.create');
+    }
+
+    public function store(Request $request)
+    {
+        // dd($request->all());
+        $request->validate([
+            'logo' => ['image', 'required', 'max:2000'],
+            'name' => ['required', 'max:200'],
+            'is_featured' => ['required'],
+            'status' => ['required'],
+        ]);
+
+        $logoPath = $this->uploadImage($request, 'logo', 'uploads');
+
+        $brand = new Brand();
+
+        $brand->logo = $logoPath;
+        $brand->name = $request->name;
+        $brand->slug = Str::slug($request->name);
+        $brand->is_featured = $request->is_featured;
+        $brand->status = $request->status;
+        $brand->save();
+
+        toastr('Created Successfully!', 'success');
+        return redirect()->route('admin.brand.index');
+    }
+
+    public function show(string $id)
+    {
+        //
+    }
+
+    public function edit(string $id)
+    {
+        //
+    }
+
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    public function destroy(string $id)
+    {
+        //
+    }
+}
