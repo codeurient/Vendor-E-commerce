532c39aa8118eedd8417dd0af82ccb9d94ae64a2
codeurient <codeurient@gmail.com>
2025-10-08 17:13:04 +0400

    Product Variant- Create necessary files

---
+++ .lessons/58 Backend Product And Related Features/19 Product Variant- Create necessary files/code/app/Http/Controllers/Backend/ProductVariantController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\DataTables\ProductVariantDataTable;
+
+class ProductVariantController extends Controller
+{
+
+    public function index(Request $request, ProductVariantDataTable $dataTable)
+    {
+        $product = Product::findOrFail($request->product);
+        return $dataTable->render('admin.product.product-variant.index', compact('product'));
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show(string $id)
+    {
+        //
+    }
+
+    public function edit(string $id)
+    {
+        //
+    }
+
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    public function destroy(string $id)
+    {
+        //
+    }
+}
