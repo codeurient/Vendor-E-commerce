7d0b9d6caff8bf434b8471a41e2d7f66877ee2d8
codeurient <codeurient@gmail.com>
2025-10-09 11:06:26 +0400

    Product Variant Item- Create necessary files

---
+++ .lessons/58 Backend Product And Related Features/24 Product Variant Item- Create necessary files/code/app/DataTables/ChildCategoryDataTable.php
@@ -0,0 +1,97 @@
+<?php
+namespace App\DataTables;
+
+use App\Models\ChildCategory;
+use Illuminate\Database\Eloquent\Builder as QueryBuilder;
+use Yajra\DataTables\EloquentDataTable;
+use Yajra\DataTables\Html\Builder as HtmlBuilder;
+use Yajra\DataTables\Html\Button;
+use Yajra\DataTables\Html\Column;
+use Yajra\DataTables\Html\Editor\Editor;
+use Yajra\DataTables\Html\Editor\Fields;
+use Yajra\DataTables\Services\DataTable;
+
+class ChildCategoryDataTable extends DataTable
+{
+
+    public function dataTable(QueryBuilder $query): EloquentDataTable
+    {
+        return (new EloquentDataTable($query))
+
+            ->addColumn('action', function($query) {
+                $editBtn   = "<a href='".route('admin.child-category.edit',      $query->id)."' class='btn btn-primary'>Edit</a>";
+                $deleteBtn = "<a href='".route('admin.child-category.destroy',   $query->id)."' class='btn btn-danger ml-2 delete-item'>Delete</a>";
+                return  $editBtn.$deleteBtn;
+            })
+            ->addColumn('status', function($query) {
+                if($query->status == 1) {
+                    $button = '<label class="custom-switch mt-2">
+                                    <input type="checkbox" checked name="custom-switch-checkbox" data-id="'.$query->id.'" class="custom-switch-input change-status">
+                                    <span class="custom-switch-indicator"></span>
+                                </label>';
+                } else {
+                    $button = '<label class="custom-switch mt-2">
+                                    <input type="checkbox" name="custom-switch-checkbox" data-id="'.$query->id.'" class="custom-switch-input change-status">
+                                    <span class="custom-switch-indicator"></span>
+                                </label>';
+                }
+                return $button;
+            })
+            
+            ->addColumn('category',      function($query) {
+                return $query->category->name;
+            })
+            ->addColumn('sub_category',  function($query) {
+                return $query->subCategory->name;
+            })
+
+            ->rawColumns(['action', 'status'])
+
+            ->setRowId('id');
+    }
+ 
+    public function query(ChildCategory $model): QueryBuilder
+    {
+        return $model->newQuery();
+    }
+
+    public function html(): HtmlBuilder
+    {
+        return $this->builder()
+                    ->setTableId('childcategory-table')
+                    ->columns($this->getColumns())
+                    ->minifiedAjax()
+                    //->dom('Bfrtip')
+                    ->orderBy(1)
+                    ->selectStyleSingle()
+                    ->buttons([
+                        Button::make('excel'),
+                        Button::make('csv'),
+                        Button::make('pdf'),
+                        Button::make('print'),
+                        Button::make('reset'),
+                        Button::make('reload')
+                    ]);
+    }
+
+    public function getColumns(): array
+    {
+        return [
+            Column::make('id'),
+            Column::make('name'),
+            Column::make('category'),
+            Column::make('sub_category'),
+            Column::make('status'),
+            Column::computed('action')
+                  ->exportable(false)
+                  ->printable(false)
+                  ->width(200)
+                  ->addClass('text-center'),
+        ];
+    }
+
+    protected function filename(): string
+    {
+        return 'ChildCategory_' . date('YmdHis');
+    }
+}
