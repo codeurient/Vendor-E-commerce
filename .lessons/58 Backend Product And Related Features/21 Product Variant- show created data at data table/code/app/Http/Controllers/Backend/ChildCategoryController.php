c5ef420c4ace4af73052a8b1177d4f8dc127f780
codeurient <codeurient@gmail.com>
2025-10-09 09:22:16 +0400

    Product Variant- show created data at data table

---
+++ .lessons/58 Backend Product And Related Features/21 Product Variant- show created data at data table/code/app/Http/Controllers/Backend/ChildCategoryController.php
@@ -0,0 +1,113 @@
+<?php
+namespace App\Http\Controllers\Backend;
+
+use Str;
+use App\Models\Category;
+use App\Models\SubCategory;
+use Illuminate\Http\Request;
+use App\Models\ChildCategory;
+use App\Http\Controllers\Controller;
+use App\DataTables\ChildCategoryDataTable;
+
+
+class ChildCategoryController extends Controller
+{
+
+    public function index(ChildCategoryDataTable $dataTable)
+    {
+        return $dataTable->render('admin.child-category.index');
+    }
+
+    public function create()
+    {
+        $categories = Category::all();
+        return view('admin.child-category.create', compact('categories'));
+    }
+
+    public function getSubCategories(Request $request)
+    {
+        $subCategories = SubCategory::where('category_id', $request->id)->where('status', 1)->get();
+        return $subCategories;
+    }
+
+    public function store(Request $request)
+    {
+        // dd($request->all());
+        $request->validate([
+            'category' => ['required'],
+            'sub_category' => ['required'],
+            'name' => ['required', 'max:200', 'unique:child_categories,name'],
+            'status' => ['required'],
+        ]);
+        
+        $childCategory = new ChildCategory();
+        
+        $childCategory->category_id = $request->category;
+        $childCategory->sub_category_id = $request->sub_category;
+        $childCategory->name = $request->name;
+        $childCategory->slug = Str::slug($request->name);
+        $childCategory->status = $request->status;
+        $childCategory->save();
+
+        toastr('Created Successfully!', 'success');
+        return redirect()->route('admin.child-category.index');
+    }
+
+    public function show(string $id)
+    {
+        //
+    }
+
+    public function edit(string $id)
+    {
+        $categories = Category::all();
+        $childCategory = ChildCategory::findOrFail($id);
+        $subCategories = SubCategory::where('category_id', $childCategory->category_id)->get();
+        return view('admin.child-category.edit', compact('categories', 'childCategory', 'subCategories'));
+    }
+
+    public function update(Request $request, string $id)
+    {
+        // dd($request->all());
+        $request->validate([
+            'category' => ['required'],
+            'sub_category' => ['required'],
+            'name' => ['required', 'max:200', 'unique:child_categories,name,'.$id],
+            'status' => ['required'],
+        ]);
+        
+        $childCategory = ChildCategory::findOrFail($id);
+        
+        $childCategory->category_id = $request->category;
+        $childCategory->sub_category_id = $request->sub_category;
+        $childCategory->name = $request->name;
+        $childCategory->slug = Str::slug($request->name);
+        $childCategory->status = $request->status;
+        $childCategory->save();
+
+        toastr('Updated Successfully!', 'success');
+        return redirect()->route('admin.child-category.index');
+    }
+
+    public function destroy(string $id)
+    {
+        $childCategory = ChildCategory::findOrFail($id);
+        $childCategory->delete();
+
+        return response(['status' => 'success', 'message' => 'Deleted Successfully!']);
+
+    }
+
+    public function changeStatus(Request $request) 
+    {
+        // dd($request->all());
+        $subCategory = ChildCategory::findOrFail($request->id);
+        $subCategory->status = $request->isChecked == 'true' ? 1 : 0;
+        $subCategory->save();
+        
+        return response(['message' => 'Status has been updated!']);
+    }
+}
+
+
+
