b046eebf2b041ba7a0926dea8bb8c5959564a64f
codeurient <codeurient@gmail.com>
2025-10-09 13:09:41 +0400

    Product Variant item- Fetch datatable data on condition

---
+++ .lessons/58 Backend Product And Related Features/27 Product Variant item- Fetch datatable data on condition/code/routes/admin.php
@@ -0,0 +1,90 @@
+<?php 
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Backend\AdminController;
+use App\Http\Controllers\Backend\BrandController;
+use App\Http\Controllers\Backend\SliderController;
+use App\Http\Controllers\Backend\ProductController;
+use App\Http\Controllers\Backend\ProfileController;
+use App\Http\Controllers\Backend\CategoryController;
+use App\Http\Controllers\Backend\SubCategoryController;
+use App\Http\Controllers\Backend\ChildCategoryController;
+use App\Http\Controllers\Backend\ProductVariantController;
+use App\Http\Controllers\Backend\AdminVendorProfileController;
+use App\Http\Controllers\Backend\ProductVariantItemController;
+use App\Http\Controllers\Backend\ProductImageGalleryController;
+use App\Http\Controllers\Admin\Auth\AdminAuthenticatedSessionController;
+
+
+Route::middleware('guest.admin')->group(function () {
+    Route::get('login', [AdminAuthenticatedSessionController::class, 'create'])->name('login');
+    Route::post('login', [AdminAuthenticatedSessionController::class, 'store'])->name('login.submit');
+});
+
+Route::middleware(['auth', 'role:admin'])->group(function () {
+    Route::get('dashboard',                  [AdminController::class, 'dashboard'])->name('dashboard');
+    Route::get('profile',                    [ProfileController::class, 'index'])->name('profile');
+    Route::post('profile/update',            [ProfileController::class, 'updateProfile'])->name('profile.update');
+    Route::post('profile/update/password',   [ProfileController::class, 'updatePassword'])->name('password.update');
+    Route::post('logout',                    [AdminAuthenticatedSessionController::class, 'destroy'])->name('logout');
+
+    // Slider route
+    Route::resource('slider', SliderController::class);
+
+    // Category route
+    Route::put('change-status', [CategoryController::class, 'changeStatus'])->name('category.change-status');
+    Route::resource('category', CategoryController::class);
+
+    // Sub Category route
+    Route::put('subcategory/change-status', [SubCategoryController::class, 'changeStatus'])->name('sub-category.change-status');
+    Route::resource('sub-category', SubCategoryController::class);
+    
+    // Child Category route
+    Route::put('child-category/change-status', [ChildCategoryController::class, 'changeStatus'])->name('child-category.change-status');
+    Route::get('get-subcategories', [ChildCategoryController::class, 'getSubCategories'])->name('get-subcategories');
+    Route::resource('child-category', ChildCategoryController::class);
+
+    // Brand Category route
+    Route::put('brand/change-status', [BrandController::class, 'changeStatus'])->name('brand.change-status');
+    Route::resource('brand', BrandController::class);
+
+    // Vendor Profile routes
+    Route::resource('vendor-profile', AdminVendorProfileController::class);
+    
+    // Product routes
+    Route::get('product/get-subcategories',     [ProductController::class, 'getSubCategories'])->name('product.get-subcategories');
+    Route::get('product/get-child-categories',  [ProductController::class, 'getChildCategories'])->name('product.get-child-categories');
+    Route::put('product/change-status',         [ProductController::class, 'changeStatus'])->name('product.change-status');
+    Route::resource('products', ProductController::class);
+
+    /** Products image gallery route */
+    Route::resource('products-image-gallery', ProductImageGalleryController::class);
+    
+    /** Products variant route */
+    Route::put('products-variant/change-status', [ProductVariantController::class, 'changeStatus'])->name('products-variant.change-status');
+    
+    Route::resource('products-variant',           ProductVariantController::class);
+    
+
+    /** Products variant item route */
+    Route::get('products-variant-item/{productId}/{variantId}',         [ProductVariantItemController::class,       'index'])->name('products-variant-item.index');
+
+
+
+    Route::get('products-variant-item/create/{productId}/{variantId}',  [ProductVariantItemController::class,      'create'])->name('products-variant-item.create');
+    Route::post('products-variant-item',                                [ProductVariantItemController::class,       'store'])->name('products-variant-item.store');
+
+    Route::get('products-variant-item-edit/{variantItemId}',            [ProductVariantItemController::class,        'edit'])->name('products-variant-item.edit');
+
+    Route::put('products-variant-item-update/{variantItemId}',          [ProductVariantItemController::class,      'update'])->name('products-variant-item.update');
+
+    Route::delete('products-variant-item/{variantItemId}',              [ProductVariantItemController::class,     'destroy'])->name('products-variant-item.destroy');
+
+    Route::put('products-variant-item-status',                          [ProductVariantItemController::class, 'chageStatus'])->name('products-variant-item.chages-status');
+
+    
+});
+
+
+
+
+
