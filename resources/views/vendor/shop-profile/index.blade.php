956d848d6fd213c9059baaa4347a989c35ce589b
codeurient <codeurient@gmail.com>
2025-12-16 17:22:35 +0400

    vendor controller

---
+++ resources/views/vendor/shop-profile/index.blade.php
@@ -0,0 +1,72 @@
+@extends('vendor.layouts.master')
+
+@section('content')
+    <section id="wsus__dashboard">
+        <div class="container-fluid">
+
+            @include('vendor.layouts.sidebar')
+
+            <div class="row">
+                <div class="col-xl-9 col-xxl-10 col-lg-9 ms-auto">
+                    <div class="dashboard_content mt-2 mt-md-0">
+                        <h3><i class="far fa-user"></i>Shop profile</h3>
+                        <div class="wsus__dashboard_profile">
+                            <div class="wsus__dash_pro_area">
+
+                                <form action="" method="POST"
+                                    enctype="multipart/form-data">
+                                    @csrf
+                                    <div class="form-group">
+                                        <label>Preview</label>
+                                        <br>
+                                        <img width="200px" src="{{ asset($profile->banner) }}" alt="">
+                                    </div>
+                                    <div class="form-group">
+                                        <label>Banner</label>
+                                        <input type="file" class="form-control" name="banner">
+                                    </div>
+                                    <div class="form-group">
+                                        <label>Phone</label>
+                                        <input type="text" class="form-control" name="phone"
+                                            value="{{ $profile->phone }}">
+                                    </div>
+                                    <div class="form-group">
+                                        <label>Email</label>
+                                        <input type="text" class="form-control" name="email"
+                                            value="{{ $profile->email }}">
+                                    </div>
+                                    <div class="form-group">
+                                        <label>Address</label>
+                                        <input type="text" class="form-control" name="address"
+                                            value="{{ $profile->address }}">
+                                    </div>
+                                    <div class="form-group">
+                                        <label>Description</label>
+                                        <textarea class="summernote" name="description">{{ $profile->description }}</textarea>
+                                    </div>
+                                    <div class="form-group">
+                                        <label>Facebook</label>
+                                        <input type="text" class="form-control" name="fb_link"
+                                            value="{{ $profile->fb_link }}">
+                                    </div>
+                                    <div class="form-group">
+                                        <label>Twitter</label>
+                                        <input type="text" class="form-control" name="tw_link"
+                                            value="{{ $profile->tw_link }}">
+                                    </div>
+                                    <div class="form-group">
+                                        <label>Instagram</label>
+                                        <input type="text" class="form-control" name="insta_link"
+                                            value="{{ $profile->insta_link }}">
+                                    </div>
+                                    <button type="submmit" class="btn btn-primary">Update</button>
+                                </form>
+                                
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
--- routes/vendor.php
+++ routes/vendor.php
@@ -2,8 +2,12 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Backend\VendorController;
 use App\Http\Controllers\Backend\VendorProfileController;
+use App\Http\Controllers\Backend\VendorShopProfileController;
 
 Route::get('dashboard', [VendorController::class, 'dashboard'])->name('dashboard');
 Route::get('profile',   [VendorProfileController::class, 'index'])->name('profile');
 Route::put('profile',    [VendorProfileController::class, 'updateProfile'])->name('profile.update');
 Route::post('profile',   [VendorProfileController::class, 'updatePassword'])->name('profile.update.password');
+
+// Vendor shop profile
+Route::resource('shop-profile', VendorShopProfileController::class);
