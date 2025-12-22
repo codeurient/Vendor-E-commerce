50cf8d81ce4c4fe8105f223183e6b0b22a7db9af
codeurient <codeurient@gmail.com>
2025-12-22 08:51:44 +0400

    Product- working with product gallery (part-2)

--- app/Http/Controllers/Backend/VendorProductImageGalleryController.php
+++ app/Http/Controllers/Backend/VendorProductImageGalleryController.php
@@ -4,12 +4,15 @@ namespace App\Http\Controllers\Backend;
 
 use App\Models\Product;
 use Illuminate\Http\Request;
+use App\Traits\ImageUploadTrait;
+use App\Models\ProductImageGallery;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use App\DataTables\VendorProductImageGalleryDataTable;
 
 class VendorProductImageGalleryController extends Controller
 {
+    use ImageUploadTrait;
 
     public function index(Request $request, VendorProductImageGalleryDataTable $dataTable)
     {
@@ -32,7 +35,22 @@ class VendorProductImageGalleryController extends Controller
 
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'image.*' => ['required', 'image', 'max:2048']
+        ]);
+
+        /** Handle image upload */
+        $imagePaths = $this->uploadMultiImage($request, 'image', 'uploads');
+
+        foreach($imagePaths as $path){
+            $productImageGallery = new ProductImageGallery();
+            $productImageGallery->image = $path;
+            $productImageGallery->product_id = $request->product;
+            $productImageGallery->save();
+        }
+
+        toastr('Uploaded successfully!');
+        return redirect()->back();
     }
 
 
@@ -56,6 +74,16 @@ class VendorProductImageGalleryController extends Controller
 
     public function destroy(string $id)
     {
-        //
+        $productImage = ProductImageGallery::findOrFail($id);
+
+        /** Check product vendor */
+        if($productImage->product->vendor_id !== Auth::user()->vendor->id){
+            abort(404);
+        }
+
+        $this->deleteImage($productImage->image);
+        $productImage->delete();
+
+        return response(['status' => 'success', 'message' => 'Deleted Successfully!']);
     }
 }
--- resources/views/vendor/layouts/master.blade.php
+++ resources/views/vendor/layouts/master.blade.php
@@ -5,6 +5,9 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no, target-densityDpi=device-dpi" />
   <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800&display=swap" rel="stylesheet">
+  
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+
   <title>One Shop || e-Commerce HTML Template</title>
   <link rel="icon" type="image/png" href="images/favicon.png">
   <link rel="stylesheet" href="{{ asset('frontend/css/all.min.css') }}">
@@ -99,13 +102,12 @@
   <script src="{{ asset('frontend/js/venobox.min.js') }}"></script>
   <!--classycountdown js-->
   <script src="{{ asset('frontend/js/jquery.classycountdown.js') }}"></script>
-
   <script src="{{ asset('backend/assets/modules/summernote/summernote-bs4.js') }}"></script>
-
   <script src="http://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
-
   <script src="{{ asset('backend/assets/modules/moment.min.js') }}"></script>
   <script src="{{ asset('backend/assets/modules/bootstrap-daterangepicker/daterangepicker.js') }}"></script>
+
+  <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
   <!--main/custom js-->
   <script src="{{ asset('frontend/js/main.js') }}"></script>
@@ -126,5 +128,54 @@
 
   @stack('scripts')
   
+  <script>
+    $(document).ready( function () {
+      $.ajaxSetup({
+        headers: {
+          'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        }
+      });
+
+      $('body').on('click', '.delete-item', function(event){
+        event.preventDefault();
+        let deleteUrl = $(this).attr('href');
+
+        Swal.fire({
+          title: "Are you sure?",
+          text: "You won't be able to revert this!",
+          icon: "warning",
+          showCancelButton: true,
+          confirmButtonColor: "#3085d6",
+          cancelButtonColor: "#d33",
+          confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+          if (result.isConfirmed) {
+            $.ajax({
+              type: 'DELETE',
+              url: deleteUrl,
+
+              success: function(data) {
+                if(data.status == 'success') {
+                  Swal.fire({ 
+                    title: "Deleted!",
+                    text: data.message
+                  });
+                  window.location.reload();
+                } else if(data.status == 'error') {
+                  Swal.fire({ 
+                    title: "Can't delete!",
+                    text: data.message
+                  });
+                }
+              },
+              error: function(xhr, status, error) { console.log(error); }
+            })
+          }
+        });
+
+      })
+    })
+  </script>
+
 </body>
 </html>
