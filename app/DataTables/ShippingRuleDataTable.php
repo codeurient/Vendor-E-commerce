ed7e4219529124ae83673052f39d5dfeec4ce428
codeurient <codeurient@gmail.com>
2025-12-31 15:51:41 +0400

    Shipping Rule - Working with create page

--- app/DataTables/ShippingRuleDataTable.php
+++ app/DataTables/ShippingRuleDataTable.php
@@ -3,40 +3,79 @@
 namespace App\DataTables;
 
 use App\Models\ShippingRule;
-use Illuminate\Database\Eloquent\Builder as QueryBuilder;
-use Yajra\DataTables\EloquentDataTable;
-use Yajra\DataTables\Html\Builder as HtmlBuilder;
+use App\Models\GeneralSetting;
 use Yajra\DataTables\Html\Button;
 use Yajra\DataTables\Html\Column;
+use Yajra\DataTables\EloquentDataTable;
 use Yajra\DataTables\Html\Editor\Editor;
 use Yajra\DataTables\Html\Editor\Fields;
 use Yajra\DataTables\Services\DataTable;
+use Yajra\DataTables\Html\Builder as HtmlBuilder;
+use Illuminate\Database\Eloquent\Builder as QueryBuilder;
 
 class ShippingRuleDataTable extends DataTable
 {
-    /**
-     * Build the DataTable class.
-     *
-     * @param QueryBuilder $query Results from query() method.
-     */
+    protected $currencyIcon = '';
+
+    public function __construct()
+    {
+        $this->currencyIcon = GeneralSetting::first()->currency_icon;
+    }
+
+    
     public function dataTable(QueryBuilder $query): EloquentDataTable
     {
         return (new EloquentDataTable($query))
-            ->addColumn('action', 'shippingrule.action')
+            ->addColumn('action', function($query){
+                $editBtn   = "<a href='".route('admin.shipping-rule.edit',    $query->id)."' class='btn btn-primary'>                  <i class='far fa-edit'>       </i></a>";
+                $deleteBtn = "<a href='".route('admin.shipping-rule.destroy', $query->id)."' class='btn btn-danger ml-2 delete-item'>  <i class='far fa-trash-alt'>  </i></a>";
+
+                return $editBtn.$deleteBtn;
+            })
+            ->addColumn('status', function($query){
+                if($query->status == 1){
+                    $button = 
+                    '<label class="custom-switch mt-2">
+                        <input type="checkbox" checked name="custom-switch-checkbox" data-id="'.$query->id.'" class="custom-switch-input change-status" >
+                        <span class="custom-switch-indicator"></span>
+                    </label>';
+                }else {
+                    $button = 
+                    '<label class="custom-switch mt-2">
+                        <input type="checkbox" name="custom-switch-checkbox" data-id="'.$query->id.'" class="custom-switch-input change-status">
+                        <span class="custom-switch-indicator"></span>
+                    </label>';
+                }
+                return $button;
+            })
+            ->addColumn('type', function($query){
+                if($query->type === 'min_cost'){
+                    return '<i class="badge badge-primary">Minimum Order Amount</i>';
+                }else {
+                    return '<i class="badge badge-success">Flate Amount</i>';
+                }
+            })
+            ->addColumn('min_cost', function($query){
+                if($query->type === 'min_cost'){
+                    return $this->currencyIcon.$query->min_cost;
+                }else {
+                    return $this->currencyIcon.'0';
+                }
+            })
+            ->addColumn('cost', function($query){
+                return $this->currencyIcon.$query->cost;
+            })
+            ->rawColumns(['status', 'action', 'type'])
             ->setRowId('id');
     }
 
-    /**
-     * Get the query source of dataTable.
-     */
+ 
     public function query(ShippingRule $model): QueryBuilder
     {
         return $model->newQuery();
     }
 
-    /**
-     * Optional method if you want to use the html builder.
-     */
+
     public function html(): HtmlBuilder
     {
         return $this->builder()
@@ -56,27 +95,26 @@ class ShippingRuleDataTable extends DataTable
                     ]);
     }
 
-    /**
-     * Get the dataTable columns definition.
-     */
+
     public function getColumns(): array
     {
         return [
-            Column::computed('action')
-                  ->exportable(false)
-                  ->printable(false)
-                  ->width(60)
-                  ->addClass('text-center'),
+
             Column::make('id'),
-            Column::make('add your columns'),
-            Column::make('created_at'),
-            Column::make('updated_at'),
+            Column::make('name'),
+            Column::make('type'),
+            Column::make('min_cost'),
+            Column::make('cost'),
+            Column::make('status'),
+            Column::computed('action')
+            ->exportable(false)
+            ->printable(false)
+            ->width(200)
+            ->addClass('text-center'),
         ];
     }
 
-    /**
-     * Get the filename for export.
-     */
+
     protected function filename(): string
     {
         return 'ShippingRule_' . date('YmdHis');
--- app/Http/Controllers/Backend/ShippingRuleController.php
+++ app/Http/Controllers/Backend/ShippingRuleController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Backend;
 
+use App\Models\ShippingRule;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\DataTables\ShippingRuleDataTable;
@@ -15,13 +16,31 @@ class ShippingRuleController extends Controller
 
     public function create()
     {
-        //
+        return view('admin.shipping-rule.create');
     }
 
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'name' => ['required', 'max:200'],
+            'type' => ['required'],
+            'min_cost' => ['nullable', 'integer'],
+            'cost' => ['required', 'integer'],
+            'status' => ['required']
+        ]);
+
+        $shipping = new ShippingRule();
+        $shipping->name = $request->name;
+        $shipping->type = $request->type;
+        $shipping->min_cost = $request->min_cost;
+        $shipping->cost = $request->cost;
+        $shipping->status = $request->status;
+        $shipping->save();
+
+        toastr('Created Successfully', 'success', 'Success');
+        return redirect()->route('admin.shipping-rule.index');
     }
+
 
     public function show(string $id)
     {
@@ -30,12 +49,30 @@ class ShippingRuleController extends Controller
 
     public function edit(string $id)
     {
-        //
+        $shipping = ShippingRule::findOrFail($id);
+        return view('admin.shipping-rule.edit', compact('shipping'));
     }
 
     public function update(Request $request, string $id)
     {
-        //
+        $request->validate([
+            'name'     => ['required', 'max:200'],
+            'type'     => ['required'],
+            'min_cost' => ['nullable', 'integer'],
+            'cost'     => ['required', 'integer'],
+            'status'   => ['required']
+        ]);
+
+        $shipping = ShippingRule::findOrFail($id);
+        $shipping->name = $request->name;
+        $shipping->type = $request->type;
+        $shipping->min_cost = $request->min_cost;
+        $shipping->cost = $request->cost;
+        $shipping->status = $request->status;
+        $shipping->save();
+
+        toastr('Updated Successfully', 'success', 'Success');
+        return redirect()->route('admin.shipping-rule.index');
     }
 
     public function destroy(string $id)
