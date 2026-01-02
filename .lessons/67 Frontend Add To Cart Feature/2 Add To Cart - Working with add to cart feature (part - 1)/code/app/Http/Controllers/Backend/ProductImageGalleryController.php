a0e9575349cafecc8507e27ea0852f19d6f877cf
codeurient <codeurient@gmail.com>
2026-01-02 09:59:11 +0400

    Add To Cart - Working with add to cart feature (part - 1)

---
+++ .lessons/67 Frontend Add To Cart Feature/2 Add To Cart - Working with add to cart feature (part - 1)/code/app/Http/Controllers/Backend/ProductImageGalleryController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use App\DataTables\ProductImageGalleryDataTable;
+use App\Http\Controllers\Controller;
+use App\Models\Product;
+use App\Models\ProductImageGallery;
+use App\Traits\ImageUploadTrait;
+use Illuminate\Http\Request;
+
+
+class ProductImageGalleryController extends Controller
+{
+    use ImageUploadTrait;
+
+    public function index(Request $request, ProductImageGalleryDataTable $dataTable)
+    {
+        $product = Product::findOrFail(   $request->product  );
+        return $dataTable->render('admin.product.image-gallery.index', compact('product'));
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'image.*' => ['required', 'image', 'max:2048']
+        ]);
+
+        /** Handle image upload */
+        $imagePaths = $this->uploadMultiImage($request, 'image', 'uploads');
+
+        foreach($imagePaths as $path){
+            $productImageGallery = new ProductImageGallery();
+            $productImageGallery->image = $path;
+            $productImageGallery->product_id = $request->product;
+            $productImageGallery->save();
+        }
+
+        toastr('Uploaded successfully!');
+        return redirect()->back();
+    }
+
+    public function show(string $id)
+    {
+        //
+    }
+    public function edit(string $id)
+    {
+        //
+    }
+
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    public function destroy(string $id)
+    {
+        $productImage = ProductImageGallery::findOrFail($id);
+        $this->deleteImage($productImage->image);
+        $productImage->delete();
+
+        return response(['status' => 'success', 'message' => 'Deleted Successfully!']);
+    }
+}
