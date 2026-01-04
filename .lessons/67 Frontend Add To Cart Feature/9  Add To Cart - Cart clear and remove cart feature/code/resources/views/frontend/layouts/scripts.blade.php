c0cc3bd4764d767113d38c8ae28e2478f41ffc5b
codeurient <codeurient@gmail.com>
2026-01-04 21:54:18 +0400

    Add To Cart - Cart clear and remove cart feature

---
+++ .lessons/67 Frontend Add To Cart Feature/9  Add To Cart - Cart clear and remove cart feature/code/resources/views/frontend/layouts/scripts.blade.php
@@ -0,0 +1,31 @@
+<script>
+    $(document).ready(function() {
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+        });
+
+        // add product into cart
+        $(document).on('submit', '.shopping-cart-form', function(e) {
+            e.preventDefault();
+            let formData = $(this).serialize();
+
+            console.log(formData);
+
+            $.ajax({
+                method: 'POST',
+                data: formData,
+                url: "{{ route('add-to-cart') }}",
+                success: function(data) {
+                    if(data.status === 'success'){
+                        toastr.success(data.message);
+                    }else if (data.status === 'error'){
+                        toastr.error(data.message);
+                    }
+                },
+                error: function(data) { }
+            })
+        })
+    })
+</script>
